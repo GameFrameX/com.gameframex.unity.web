0937eee00099ec69960721460b0e5803bab1c529
Blank <wangfj11@foxmail.com>
2024-04-09 00:50:36 +0800

    [拆分]1. 拆分组件

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿## HOMEPAGE
+GameFrameX 的Web 请求组件
+
+ **Web 请求 组件 (Web Request)** - 提供使用短连接的功能，可以用 Get 或者 Post 方法向服务器发送请求并获取响应数据，可指定允许几个 Web 请求器进行同时请求。
+
+
+# 使用文档
+
+暂时没写
---
+++ Runtime/GameFrameXCroppingHelper.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace GameFrameX.Web.Runtime
+{
+    public class GameFrameXCroppingHelper : MonoBehaviour
+    {
+        private void Start()
+        {
+            _ = typeof(WebComponent);
+            _ = typeof(IWebManager);
+            _ = typeof(WebManager);
+        }
+    }
+}
